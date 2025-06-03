24e0b98d37e9915f576ec8ac18439bb6df1f5233
Deepak kumar sharma <medksharma017@gmail.com>
2025-06-03 15:08:22 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    public static void main(String[] args) {
+        int n1=20;
+        int n2=30;
+        int sum=n1+n2;
+        System.out.println("Sum is :"  +sum);
+    }
+
+}
---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 25;
+        int sum = number1 + number2;
+        System.out.println("The sum is: " + sum); // Output: The sum is: 35
+    }
+}
